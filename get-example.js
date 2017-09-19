96089e2ee4f55fcd1c33d7311a8e3935fb069aa0
AJ ONeal <aj@daplie.com>
2017-09-19 15:24:21 -0600

    update sources
    Author:    AJ ONeal <coolaj86@gmail.com>

--- get-example.js
+++ get-example.js
@@ -1,22 +1,22 @@
 /*jshint strict:true node:true es5:true onevar:true laxcomma:true laxbreak:true eqeqeq:true immed:true latedef:true*/
 (function () {
-  "use strict";
+'use strict';
 
-  var http = require('http')
-    , path = require('path')
-    , fs = require('fs')
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+var url = 'http://pastebin.com/raw/zDN3VPQZ';
+
+http.get(url, function (res) {
+  var text = ''
     ;
 
-  http.get('http://pastebin.com/raw.php?i=zDN3VPQZ', function (res) {
-    var text = ''
-      ;
-
-    res.on('data', function (buf) {
-      text += buf.toString('utf8');
-    });
-    res.on('end', function () {
-      fs.writeFileSync(path.join(__dirname, 'questionnaire.json'), text, 'utf8');
-    });
+  res.on('data', function (buf) {
+    text += buf.toString('utf8');
   });
+  res.on('end', function () {
+    fs.writeFileSync(path.join(__dirname, 'questionnaire.json'), text, 'utf8');
+  });
+});
 
 }());
